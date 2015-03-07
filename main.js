994e7196ee8a362c46de57f8f70bc7accc2e6f69
calumacutt <calumacutt@hotmail.com>
2015-03-07 18:31:44 +1000

    Started super efficientizing things

--- main.js
+++ main.js
@@ -1,63 +1,61 @@
 var MoneyDisplayTimer;
 var MoneyIncTimer;
 
-MoneyIncTimer = setInterval(function(){cookieIncrement()}, 10);
-MoneyDisplayTimer = setInterval(function(){cookieDisplay(cookies)}, 10);
+MoneyIncTimer = setInterval(function(){MoneyInc()}, 10);
+MoneyDisplayTimer = setInterval(function(){MoneyDisplay(Money)}, 10);
 
-var cursorinitialcost = 10;
+var PennyHelpersIniCost = 10;
 var friendinitialcost = 100;
 var expgrowthconst = 1.1;
-var cookieincrementrate = 0; //increment rate in cookies/second
+var MoneyIncRate = 0; //increment rate in dollars/second
 
+var Money = 0;
 
-
-var cookies = 0;
-
-function cookieIncrement(){
-    cookies = cookies + cookieincrementrate*0.01;
+function MoneyInc(){
+    Money = Money + MoneyIncRate*0.01;
 };
 
-function cookieDisplay(){
-    document.getElementById("cookies").innerHTML = Math.floor(cookies);
+function MoneyDisplay(){
+    document.getElementById("Money").innerHTML = Math.floor(Money);
 };
 
-function cookieClick(number){
-	cookies = cookies + number;
+function PennyClick(number){
+	Money = Money + number;
 }
 
-var cursors = 0;
+var PennyHelpers = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(cursorinitialcost * Math.pow(expgrowthconst,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-		cookieincrementrate = cookieincrementrate + 1;
+function buyPennyHelpers(){
+    var PennyHelperCost = Math.floor(PennyHelpersIniCost * Math.pow(expgrowthconst,PennyHelpers));     //works out the cost of this cursor
+    if(Money >= PennyHelperCost){                                   //checks that the player can afford the cursor
+        PennyHelpers = PennyHelpers + 1;                                   //increases number of PennyHelpers
+    	Money = Money - PennyHelperCost;                          //removes the Money spent
+        document.getElementById('PennyHelpers').innerHTML = PennyHelpers;  //updates the number of PennyHelpers for the user
+		MoneyIncRate = MoneyIncRate + 1;
     };
-    var nextCost = Math.floor(cursorinitialcost * Math.pow(expgrowthconst,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user	
+    var nextCost = Math.floor(PennyHelpersIniCost * Math.pow(expgrowthconst,PennyHelpers));       //works out the cost of the next cursor
+    document.getElementById('PennyHelperCost').innerHTML = nextCost;  //updates the cursor cost for the user	
 };
 
 var friends = 0;
 
 function buyFriend(){
+
     var friendCost = Math.floor(friendinitialcost * Math.pow(expgrowthconst,friends));     //works out the cost of this cursor
-    if(cookies >= friendCost){                                   //checks that the player can afford the cursor
-        friends = friends + 1;                                   //increases number of cursors
-    	cookies = cookies - friendCost;                          //removes the cookies spent
-        document.getElementById('friends').innerHTML = friends;  //updates the number of cursors for the user
-		cookieincrementrate = cookieincrementrate + 10;
+    if(Money >= friendCost){                                   //checks that the player can afford the cursor
+        friends = friends + 1;                                   //increases number of PennyHelpers
+    	Money = Money - friendCost;                          //removes the Money spent
+        document.getElementById('friends').innerHTML = friends;  //updates the number of PennyHelpers for the user
+		MoneyIncRate = MoneyIncRate + 10;
     };
     var nextCost = Math.floor(friendinitialcost * Math.pow(expgrowthconst,friends));       //works out the cost of the next cursor
     document.getElementById('friendCost').innerHTML = nextCost;  //updates the cursor cost for the user	
 };
 
-function save(){
-	var save = {
-		cookies: cookies,
-		cursors: cursors,
-		prestige: prestige
-	}
-	localStorage.setItem("save",JSON.stringify(save));
+var RateIncArray = {};
+RateIncArray["PennyHelper"] = 1;
+RateIncArray["SausageSizzle"] = 10;
+
+function BuyRateIncrease(Item){
+
 }
