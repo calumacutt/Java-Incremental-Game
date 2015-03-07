d3f59db1fbdf3d44a8ea9d1c10b05f7229587da9
calumacutt <calumacutt@hotmail.com>
2015-03-07 13:02:42 +1000

    Added Base Files
    Laying Foundations

---
+++ main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="interface.css" />
+  <title>Page Title</title>
+</head>
+
+<body>
+  <h1>This is a Heading</h1>
+  <p>This is a paragraph.</p>
+  <script type="text/javascript" src="main.js"></script>
+</body>
+
+</html>
